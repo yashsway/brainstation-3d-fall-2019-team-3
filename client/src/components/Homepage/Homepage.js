aab045fb391f930cd155aaaebc241dad5a00b670
Eric Thorfinnson <ethorf@gmail.com>
2019-11-22 11:52:24 -0500

    added data

--- client/src/components/Homepage/Homepage.js
+++ client/src/components/Homepage/Homepage.js
@@ -1,6 +1,19 @@
 import React, { Component } from 'react'
 
 export class Homepage extends Component {
+
+     data = [
+        {name:"small" ,
+         value:1104 ,
+        percentage:46   },
+        {name:"medium" ,
+        value:596 ,
+        percentage:24.8   },
+        {name:"large" ,
+        value:701 ,
+        percentage:29.2   }
+    ]
+    
     render() {
         return (
             <div>
---
+++ client/src/data.js
@@ -0,0 +1,14 @@
+let data = [
+    {name:"small" ,
+     value:1104 ,
+    percentage:46   },
+    {name:"medium" ,
+    value:596 ,
+    percentage:24.8   },
+    {name:"large" ,
+    value:701 ,
+    percentage:29.2   }
+]
+
+
+
